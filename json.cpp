3fab7155f747c3da1127eafa25b27c85e93f9dc9
hkh <1103490628@qq.com>
2020-11-16 13:54:16 +0800

    测试http+json1,bug3

--- json.cpp
+++ json.cpp
@@ -8,37 +8,61 @@
 
 using namespace httplib;
 using namespace std;
-bool stop = false;
+
 int main(void) {
-    httplib::Server svr;
-    svr.Post("/hi", [](const Request &req, Response &res) {
-        string json = R"[=======]({
-        "sourceLevel1": "PL0001",
-        "sourceLevel2": "BF0001",
-        "sourceLevel3": "BC0001",
-        "startTag": true})[=======]";
-        Json::Reader reader;
-        Json::Value root;
-        if (!reader.parse(json, root))
-        {
-            cout << "reader parse error: " << strerror(errno) << endl;
-            return -1;
-        }
-        string sourceLevel1, sourceLevel2, sourceLevel3;
-        bool startTag;
+    string json = R"[=======]({"sourceLevel1": "PL0001","sourceLevel2": "BF0001","sourceLevel3": "BC0001","startTag": true})[=======]";
+    Json::Reader reader;
+    Json::Value root;
+    if (!reader.parse(json, root))
+    {
+        cout << "reader parse error: " << strerror(errno) << endl;
+        return -1;
+    }
+    string sourceLevel1, sourceLevel2, sourceLevel3;
+    bool startTag;
 
-        sourceLevel1 = root["sourceLevel1"].asString();
-        sourceLevel2 = root["sourceLevel2"].asString();
-        sourceLevel3 = root["sourceLevel3"].asString();
-        startTag = root["startTag"].asBool();
+    sourceLevel1 = root["sourceLevel1"].asString();
+    sourceLevel2 = root["sourceLevel2"].asString();
+    sourceLevel3 = root["sourceLevel3"].asString();
+    startTag = root["startTag"].asBool();
 
-        cout << "sourceLevel1: " << sourceLevel1 << endl;
-        cout << "sourceLevel1: " << sourceLevel1 << endl;
-        cout << "sourceLevel1: " << sourceLevel1 << endl;
-        cout << "startTag: " << startTag << endl;
-        res.set_content(json, "text/plain");
-    });
-
-    std::cout << "start server..." << std::endl;
-    svr.listen("0.0.0.0", 8080);
+    cout << "sourceLevel1: " << sourceLevel1 << endl;
+    cout << "sourceLevel1: " << sourceLevel1 << endl;
+    cout << "sourceLevel1: " << sourceLevel1 << endl;
+    cout << "startTag: " << startTag << endl;
 }
+
+// bool stop = false;
+// int main(void) {
+//     httplib::Server svr;
+//     svr.Post("/hi", [](const Request &req, Response &res) {
+//         string json = R"[=======]({
+//         "sourceLevel1": "PL0001",
+//         "sourceLevel2": "BF0001",
+//         "sourceLevel3": "BC0001",
+//         "startTag": true})[=======]";
+//         Json::Reader reader;
+//         Json::Value root;
+//         if (!reader.parse(json, root))
+//         {
+//             cout << "reader parse error: " << strerror(errno) << endl;
+//             return -1;
+//         }
+//         string sourceLevel1, sourceLevel2, sourceLevel3;
+//         bool startTag;
+
+//         sourceLevel1 = root["sourceLevel1"].asString();
+//         sourceLevel2 = root["sourceLevel2"].asString();
+//         sourceLevel3 = root["sourceLevel3"].asString();
+//         startTag = root["startTag"].asBool();
+
+//         cout << "sourceLevel1: " << sourceLevel1 << endl;
+//         cout << "sourceLevel1: " << sourceLevel1 << endl;
+//         cout << "sourceLevel1: " << sourceLevel1 << endl;
+//         cout << "startTag: " << startTag << endl;
+//         res.set_content(json, "text/plain");
+//     });
+
+//     std::cout << "start server..." << std::endl;
+//     svr.listen("0.0.0.0", 8080);
+// }
